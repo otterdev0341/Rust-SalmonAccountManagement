5d5e740f2d6c69f54c60469da8e13bc2a84eb473
Otterdev_0341 <128258559+otterdev0341@users.noreply.github.com>
2025-02-25 00:56:17 +0700

    done with sign-up

--- double_entry_backend/src/infrastructure/handler/validate_util/auth_validate.rs
+++ double_entry_backend/src/infrastructure/handler/validate_util/auth_validate.rs
@@ -23,9 +23,9 @@ use crate::domain::dto::auth_dto::ReqCreateUserDto;
 #[derive(Debug, Error)]
 pub enum ValidateReqCreateUserDtoError{
     #[error("Empty String not allowed")]
-    EmptyString,
+    EmptyString(String),
     #[error("Invalid Length of data")]
-    LengthError,
+    LengthError(String),
     #[error("Invalid Email")]
     InvalidEmail,
     
@@ -33,31 +33,31 @@ pub enum ValidateReqCreateUserDtoError{
 
 pub fn check_req_create_user_dto(req_create_user_dto: &ReqCreateUserDto) -> Result<(), ValidateReqCreateUserDtoError> {
     if req_create_user_dto.username.is_empty() {
-        return Err(ValidateReqCreateUserDtoError::EmptyString);
+        return Err(ValidateReqCreateUserDtoError::EmptyString("username field must not be empty".to_string()));
     }
     if req_create_user_dto.first_name.is_empty() {
-        return Err(ValidateReqCreateUserDtoError::EmptyString);
+        return Err(ValidateReqCreateUserDtoError::EmptyString("first name field must not be empty".to_string()));
     }
     if req_create_user_dto.last_name.is_empty() {
-        return Err(ValidateReqCreateUserDtoError::EmptyString);
+        return Err(ValidateReqCreateUserDtoError::EmptyString("last name field must not be empty".to_string()));
     }
     if req_create_user_dto.email.is_empty() {
-        return Err(ValidateReqCreateUserDtoError::EmptyString);
+        return Err(ValidateReqCreateUserDtoError::EmptyString("email field must not be empty".to_string()));
     }
     if req_create_user_dto.password.is_empty() {
-        return Err(ValidateReqCreateUserDtoError::EmptyString);
+        return Err(ValidateReqCreateUserDtoError::EmptyString("password field must not be empty".to_string()));
     }
     if req_create_user_dto.username.len() < 3 || req_create_user_dto.username.len() > 15 {
-        return Err(ValidateReqCreateUserDtoError::LengthError);
+        return Err(ValidateReqCreateUserDtoError::LengthError("username must be between 3 and 15 characters".to_string()));
     }
     if req_create_user_dto.first_name.len() < 3 || req_create_user_dto.first_name.len() > 20 {
-        return Err(ValidateReqCreateUserDtoError::LengthError);
+        return Err(ValidateReqCreateUserDtoError::LengthError("first name must be between 3 and 20 characters".to_string()));
     }
     if req_create_user_dto.last_name.len() < 3 || req_create_user_dto.last_name.len() > 20 {
-        return Err(ValidateReqCreateUserDtoError::LengthError);
+        return Err(ValidateReqCreateUserDtoError::LengthError("last name must be between 3 and 20 characters".to_string()));
     }
     if req_create_user_dto.password.len() < 6 || req_create_user_dto.password.len() > 15 {
-        return Err(ValidateReqCreateUserDtoError::LengthError);
+        return Err(ValidateReqCreateUserDtoError::LengthError("password must be at least 6 characters to 15 characters".to_string()));
     }
     if !req_create_user_dto.email.contains("@") {
         return Err(ValidateReqCreateUserDtoError::InvalidEmail);
--- double_entry_backend/src/infrastructure/mysql/repositories/impl_auth_repository.rs
+++ double_entry_backend/src/infrastructure/mysql/repositories/impl_auth_repository.rs
@@ -1,11 +1,13 @@
+use core::hash;
 use std::sync::Arc;
 
 use rocket::fairing::Info;
 use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter, Set};
 use sea_orm_migration::async_trait;
 use tracing::{info, warn};
+use utoipa::openapi::info;
 
-use crate::{domain::{dto::auth_dto::{ReqCreateUserDto, ReqSignInDto, ResEntryUserDto}, entities::user, repository::require_implementation::trait_auth::AuthRepoReqImpl}, infrastructure::{argon_hash::hash_util::hash_password, handler::operation_status::auth_error::{CreateUserError, SignInError}}};
+use crate::{domain::{dto::auth_dto::{ReqCreateUserDto, ReqSignInDto, ResEntryUserDto}, entities::user, repository::require_implementation::trait_auth::AuthRepoReqImpl}, infrastructure::{argon_hash::hash_util::{hash_password, HashOperationError}, handler::operation_status::auth_error::{CreateUserError, SignInError}}};
 
 
 
@@ -25,7 +27,8 @@ impl ImplAuthRepository {
 #[async_trait::async_trait]
 impl AuthRepoReqImpl for ImplAuthRepository {
     async fn create_user(&self, user_data: ReqCreateUserDto) -> Result<(), CreateUserError> {
-        
+        info!(">>>>>>>>>>>> create user in repository");
+        info!(">>>>>>>>>>>>> check is  user exists");
         // check username is already exists
         match user::Entity::find().filter(user::Column::Username.eq(user_data.username.clone())).one(&*self.db).await {
             Ok(user) => {
@@ -36,6 +39,7 @@ impl AuthRepoReqImpl for ImplAuthRepository {
             Err(_) => ()
         }
         
+        info!(">>>>>>>>>>>>> check is email already exists");
         // check is email already exists
         match user::Entity::find().filter(user::Column::Email.eq(user_data.email.clone())).one(&*self.db).await {
             Ok(data) => {
@@ -46,29 +50,38 @@ impl AuthRepoReqImpl for ImplAuthRepository {
             Err(_) => ()
         }
         
+        info!(">>>>>>>>>>>>> create hash password");
         // create hash password from user data
-        let hash_password = hash_password(&user_data.password).unwrap();
-        
+        let hash_password = hash_password(&user_data.password).map_err(|_| HashOperationError::FailToHashPassword);
+        if hash_password.is_err() {
+            return Err(CreateUserError::InternalServerError)
+        }
+
+        info!(">>>>>>>>>>>>> create user entity");
         // create use entity to persit to database
         let new_user = user::ActiveModel {
             username: Set(user_data.username),
             email: Set(user_data.email),
-            password_hash: Set(hash_password),
+            password_hash: Set(hash_password.unwrap()),
             first_name: Set(user_data.first_name),
             last_name: Set(user_data.last_name),
             ..Default::default()
         };
         
+        info!(">>>>>>>>>>>>> insert user entity");
         // check insert result
         let persist_result = user::Entity::insert(new_user)
-            .exec(&*self.db)
-            .await
-            .map_err(|err| {
-                warn!("{}", err.to_string());
-                CreateUserError::InternalServerError
-            })?;
+            .exec_without_returning(&*self.db)
+            .await;
    
-            
+        info!(">>>>>>>>>>>>> check insert result");
+        match persist_result {
+            Ok(data) => (),
+            Err(_) => {
+                warn!("Failed to insert user entity");
+                return Err(CreateUserError::InternalServerError)
+            }
+        }
         Ok(())
         
 
